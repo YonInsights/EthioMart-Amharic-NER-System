ac64af09a2e2e9782ec577842a2be37c4b315cee
Yoninsight <yo2120018@gmail.com>
2025-02-03 17:43:51 +0300

    delete

--- src/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI(title="Telegram Data API", description="API for accessing Telegram data and object detection results.")
-
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the Telegram Data API!"}
